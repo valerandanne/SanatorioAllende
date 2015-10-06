2152da347b82a5140b10caee90de4f538f83ac12
agusdemalde <agus.demalde@gmail.com>
2015-10-06 12:23:43 -0300

    listita babyss

--- SAllende/app/src/main/java/com/example/agustina/sallende/Main.java
+++ SAllende/app/src/main/java/com/example/agustina/sallende/Main.java
@@ -1,5 +1,6 @@
 package com.example.agustina.sallende;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,44 +8,65 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 
 public class Main extends AppCompatActivity{
 
-    Button b1, b2, b3;
 
+    AlertDialog alertDialogEspe;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        View.OnClickListener handler = new View.OnClickListener() {
+            public void onClick(View v) {
+                switch(v.getId()){
+                    case R.id.button_esp:
+                        ShowEspe();
+                        break;
 
-        b1 = (Button) findViewById(R.id.button_ub);
-        b2 = (Button) findViewById(R.id.button_cob);
-        b3 = (Button) findViewById(R.id.button_esp);
-
-
-        b1.setOnClickListener(myhandler);
-        b2.setOnClickListener(myhandler);
-        b3.setOnClickListener(myhandler);
-    }
-
-    View.OnClickListener myhandler = new View.OnClickListener() {
-        public void onClick(View v) {
-            Intent in = new Intent();
-            switch (v.getId()) {
-                case R.id.button_ub:
-                    in = new Intent(Main.this, MapsActivity.class);
-                    break;
-                case R.id.button_esp:
-                    in = new Intent(Main.this, EspecialidadesActivity.class);
-                    break;
-                case R.id.button_cob:
-                    in = new Intent(Main.this, CoberturaActivity.class);
-                    break;
-                case (R.id.button_tur):
+                }
             }
-            startActivity(in);
-        }
-    };
+
+
+        };
+        findViewById(R.id.button_esp).setOnClickListener(handler);
+    }
+    public void ShowEspe() {
+        ObjectItem[] ObjectItemData = new ObjectItem[15];
+
+        ObjectItemData[0] = new ObjectItem(0, "Alergia e inmunología");
+        ObjectItemData[1] = new ObjectItem(1, "Anestesiología");
+        ObjectItemData[2] = new ObjectItem(2, "Cardiología");
+        ObjectItemData[3] = new ObjectItem(3, "Cirugía cardíaca");
+        ObjectItemData[4] = new ObjectItem(4, "Dermatología");
+        ObjectItemData[5] = new ObjectItem(5, "Fonoaudiología");
+        ObjectItemData[6] = new ObjectItem(6, "Endocrinología");
+        ObjectItemData[7] = new ObjectItem(7, "Gatroenterología");
+        ObjectItemData[8] = new ObjectItem(8, "Nefrología");
+        ObjectItemData[9] = new ObjectItem(9, "Neurología");
+        ObjectItemData[10] = new ObjectItem(10, "Nutrición");
+        ObjectItemData[11] = new ObjectItem(11, "Oftalmología");
+        ObjectItemData[12] = new ObjectItem(12, "Otorrinolaringologia");
+        ObjectItemData[13] = new ObjectItem(13, "Pediatría");
+        ObjectItemData[14] = new ObjectItem(14, "Traumatología");
+
+
+        ArrayAdapterItem adapter = new ArrayAdapterItem(this, R.layout.activity_especialidades, ObjectItemData);
+
+        ListView listViewItems = new ListView(this);
+        listViewItems.setAdapter(adapter);
+        listViewItems.setOnItemClickListener(new OnItemClickListenerListViewItem());
+
+     alertDialogEspe = new AlertDialog.Builder(this)
+             .setView(listViewItems)
+                .setTitle("ESPECIALIDADES")
+                .show();
+
+
+
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
---
+++ SAllende/app/src/main/java/com/example/agustina/sallende/ObjectItem.java
@@ -0,0 +1,17 @@
+package com.example.agustina.sallende;
+
+/**
+ * Created by Agustina on 06/10/2015.
+ */
+public class ObjectItem {
+
+    public int itemId;
+    public String itemName;
+
+    public ObjectItem(int itemId, String itemName) {
+
+        this.itemId = itemId;
+        this.itemName = itemName;
+
+    }
+}
