4012e2843260c1d0cd4d5419e45ecbcb91586bf9
beluramondelli <belu.ramondelli@gmail.com>
2015-10-08 18:38:40 -0300

    db

--- SAllende/app/src/main/java/com/example/agustina/sallende/EspecialidadesActivity.java
+++ SAllende/app/src/main/java/com/example/agustina/sallende/EspecialidadesActivity.java
@@ -1,17 +1,41 @@
 package com.example.agustina.sallende;
 
+import android.app.ListActivity;
+import android.content.CursorLoader;
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
-public class EspecialidadesActivity extends AppCompatActivity {
+public class EspecialidadesActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_especialidades);
+
+        SQLiteDB db = new SQLiteDB(this);
+        Cursor cursor= db.getAllEspecialidades();
+        String[] from = new String[]{"name"};
+        int[] to = new int[]{R.id.text};
+
+       /* SimpleCursorAdapter cursorAdapter = new SimpleCursorAdapter(this, R.layout.Row , cursor, from, to);
+
+        listViewitems.setAdapter(cursorAdapter);
+
+
+        ArrayAdapterItem adapter = new ArrayAdapterItem(this, R.layout.activity_especialidades, ObjectItemData);
+
+        ListView listViewItems = new ListView(this);
+        listViewItems.setAdapter(adapter);
+        listViewItems.setOnItemClickListener(new OnItemClickListenerListViewItem());
+        */
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
