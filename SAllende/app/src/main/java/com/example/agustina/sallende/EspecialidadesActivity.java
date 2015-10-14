461e150e2fc2883351690de4a1e46b7429335ade
agusdemalde <agus.demalde@gmail.com>
2015-10-14 18:28:12 -0300

    MIS CAMBIOS Y LOS DE BEL

--- SAllende/app/src/main/java/com/example/agustina/sallende/EspecialidadesActivity.java
+++ SAllende/app/src/main/java/com/example/agustina/sallende/EspecialidadesActivity.java
@@ -1,39 +1,17 @@
 package com.example.agustina.sallende;
 
 import android.app.Activity;
-import android.app.ListActivity;
-import android.content.CursorLoader;
+import android.content.Context;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.RelativeLayout;
-import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
-import java.sql.Array;
-import java.sql.Blob;
-import java.sql.CallableStatement;
-import java.sql.Clob;
-import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.NClob;
-import java.sql.PreparedStatement;
-import java.sql.SQLClientInfoException;
-import java.sql.SQLException;
-import java.sql.SQLWarning;
-import java.sql.SQLXML;
-import java.sql.Savepoint;
-import java.sql.Statement;
-import java.sql.Struct;
 import java.util.ArrayList;
-import java.util.Map;
-import java.util.Properties;
 
 import Beans.BeanEspecialidad;
 
@@ -53,40 +31,17 @@ public class EspecialidadesActivity extends Activity {
         Cursor c = db.getAllEspecialidades();
 
 
-
         for(c.moveToFirst(); !c.isAfterLast(); c.moveToNext())
         {
-            lista.add(especialidad = new BeanEspecialidad(c.getString(1)));
+            lista.add(especialidad = new BeanEspecialidad(c.getString(0),c.getString(1)));
         }
 
         c.close();
 
         ArrayAdapter<BeanEspecialidad> adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,lista);
         listViewItems.setAdapter(adapter);
-
-
-
-       }
-
-        /*SQLiteDB db = new SQLiteDB(this);
-        Cursor cursor= db.getAllEspecialidades();
-        String[] from = new String[]{"name"};
-        int[] to = new int[]{R.id.text};
-
-       /*SimpleCursorAdapter cursorAdapter = new SimpleCursorAdapter(this, R.layout.Row , cursor, from, to);
-
-        listViewitems.setAdapter(cursorAdapter);
-
-
-        ArrayAdapterItem adapter = new ArrayAdapterItem(this, R.layout.activity_especialidades, ObjectItemData);
-
-        ListView listViewItems = new ListView(this);
-        listViewItems.setAdapter(adapter);
         listViewItems.setOnItemClickListener(new OnItemClickListenerListViewItem());
-        */
-
-
-
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -109,4 +64,5 @@ public class EspecialidadesActivity extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
--- SAllende/app/src/main/java/com/example/agustina/sallende/SQLiteDB.java
+++ SAllende/app/src/main/java/com/example/agustina/sallende/SQLiteDB.java
@@ -18,6 +18,7 @@ public class SQLiteDB extends SQLiteOpenHelper {
     static final String colMedID="id_med";
     static  final String colMedEspe= "Especialidades";
     static final String colMedName="nombre";
+    static final String colMedSuc="Sucursal";
 
     static final String EspeTable="Especialidades";
     static final String colEspeID= "id_espe";
@@ -27,9 +28,16 @@ public class SQLiteDB extends SQLiteOpenHelper {
     static final String colCobID="id_cob";
     static final String colCobName="nombre";
 
+    static final String SucTable="Sucursal";
+    static final String colSucID="id_suc";
+    static final String colSucName="nombre";
+
     static final String viewMed= "ViewMedicos";
     static final String viewEspe="ViewEspecialidades";
     static final String viewCobertura="ViewCobertura";
+    static final String viewSucursales="ViewSucursales";
+
+
 
     public SQLiteDB(Context context) {
 
@@ -41,18 +49,26 @@ public class SQLiteDB extends SQLiteOpenHelper {
         db.execSQL("CREATE TABLE "+ EspeTable + "(" + colEspeID +  " INTEGER PRIMARY KEY AUTOINCREMENT , "
                 + colEspeDescrip + " TEXT )");
 
+        db.execSQL("CREATE TABLE "+ SucTable + "(" + colSucID +  " INTEGER PRIMARY KEY AUTOINCREMENT , "
+                + colSucName + " TEXT )");
+
         db.execSQL("CREATE TABLE " + MedTable + "(" + colMedID + " INTEGER PRIMARY KEY AUTOINCREMENT , "
-                + colMedName + " TEXT, " + colMedEspe + " INTEGER, "+ " FOREIGN KEY (" +
-                colMedEspe + ") REFERENCES " + EspeTable + " (" + colEspeID + "));");
+                + colMedName + " TEXT, " + colMedEspe + " INTEGER, "+ colMedSuc + " INTEGER, " + " FOREIGN KEY (" +
+                colMedEspe + ") REFERENCES " + EspeTable + " (" + colEspeID + ")" + ", FOREIGN KEY (" +
+                colMedSuc + ") REFERENCES " + SucTable + " (" + colSucID + "));");
 
         db.execSQL("CREATE TRIGGER fk_medEspe_espeid " + " BEFORE INSERT " + " ON " + MedTable +
                 " FOR EACH ROW BEGIN" + " SELECT CASE WHEN ((SELECT " + colEspeID + " FROM " + EspeTable +
                 " WHERE " + colEspeID + "=new." + colMedEspe + " ) IS NULL)" +
                 " THEN RAISE (ABORT,'Foreign Key Violation') END;" + "  END;");
 
+        db.execSQL("CREATE TRIGGER fk_medSuc_Sucid " + " BEFORE INSERT " + " ON " + MedTable +
+                " FOR EACH ROW BEGIN" + " SELECT CASE WHEN ((SELECT " + colSucID + " FROM " + SucTable +
+                " WHERE " + colSucID + "=new." + colMedSuc + " ) IS NULL)" +
+                " THEN RAISE (ABORT,'Foreign Key Violation') END;" + "  END;");
 
         db.execSQL("CREATE TABLE " + CobTable + "(" + colCobID + " INTEGER PRIMARY KEY AUTOINCREMENT , "
-                    + colCobName + " TEXT )");
+                + colCobName + " TEXT )");
 
         db.execSQL("CREATE VIEW " + viewMed + " AS SELECT " + MedTable + "." + colMedID +
                 " AS _id," + " " + MedTable + "." + colMedName + "," + " " + EspeTable + "."
@@ -65,107 +81,12 @@ public class SQLiteDB extends SQLiteOpenHelper {
         db.execSQL("CREATE VIEW " + viewCobertura + " AS SELECT " + CobTable + "." + colCobID +
                 " AS _id," + " " + CobTable + "." + colCobName + " FROM " + CobTable);
 
+        db.execSQL("CREATE VIEW " + viewSucursales + " AS SELECT " + SucTable + "." + colSucID +
+                " AS _id," + " " + SucTable + "." + colSucName + " FROM " + SucTable);
 
 
-        InsertMedico(db, 1, "Dr. Copioli Carlos", 1);
-        InsertMedico(db, 2, "Dra. Barrera Roxana", 1);
-        InsertMedico(db, 3, "Dra. Cavallo Marta", 1);
-        InsertMedico(db, 4, "Dra.Puentes Laura", 1);
-        InsertMedico(db, 5, "Dr. Astore Fabian",2);
-        InsertMedico(db, 6, "Dr. Demaria Javier", 2);
-        InsertMedico(db, 7, "Dr. Escudero Juan", 2);
-        InsertMedico(db, 8, "Dr. Gross Carlos", 2);
-        InsertMedico(db, 9, "Dr. Pacheco Eduardo", 2);
-        InsertMedico(db, 10, "Dr. Riveri Fancisco", 2);
-        InsertMedico(db, 11, "Dr. Salas Marcos", 2);
-        InsertMedico(db, 12, "Dra. Castro Maria Belen", 2);
-        InsertMedico(db, 13, "Dr. Moreira Eduardo", 3);
-        InsertMedico(db, 14, "Dr. Alday Luis", 3);
-        InsertMedico(db, 15, "Dr. Allall Oscar", 3);
-        InsertMedico(db, 16, "Dr. Allub Alejandro", 3);
-        InsertMedico(db, 17, "Dra. Auad Luciana", 3);
-        InsertMedico(db, 18, "Dr. Bass Gabriel", 3);
-        InsertMedico(db, 19, "Dr. Bono Julio", 3);
-        InsertMedico(db, 20, "Dr. Cordoba Roque", 4);
-        InsertMedico(db, 21, "Dr. Figueroa Marcelo", 4);
-        InsertMedico(db, 22, "Dr. Bronzi Jorge", 4);
-        InsertMedico(db, 23, "Dr. Cordoba Ignacio", 4);
-        InsertMedico(db, 24, "Dr. Liendo Rocardo", 4);
-        InsertMedico(db, 25, "Dr. Bono Julio", 5);
-        InsertMedico(db, 26, "Dra. Gonzales Eleonora", 5);
-        InsertMedico(db, 27, "Dra. Baldrini Maria Pia", 5);
-        InsertMedico(db, 28, "Dr. Centeno Alejandro", 5);
-        InsertMedico(db, 29, "Dr. Perrotat Pedro", 5);
-        InsertMedico(db, 30, "Dra. Ramona Griselda", 5);
-        InsertMedico(db, 31, "Dra. Vidal Graciaela", 5);
-        InsertMedico(db, 32, "Lic. Riera Monica", 6);
-        InsertMedico(db, 33, "Lic. Fuentes Anabella", 6);
-        InsertMedico(db, 34, "Lic. Leon Noelia", 6);
-        InsertMedico(db, 35, "Lic. Maranzana Melisa", 6);
-        InsertMedico(db, 36, "Dr. Ortiz Gustavo", 7);
-        InsertMedico(db, 37, "Dra. Arja Ana", 7);
-        InsertMedico(db, 38, "Dra. Bergoglio Marina", 7);
-        InsertMedico(db, 39, "Dra. Griguol Sandra", 7);
-        InsertMedico(db, 40, "Dra. Acuña Valeria", 7);
-        InsertMedico(db, 41, "Dr. Carri Julio", 8);
-        InsertMedico(db, 42, "Dra. Cordoba Claudia", 8);
-        InsertMedico(db, 43, "Dr. Faule Sebastian", 8);
-        InsertMedico(db, 44, "Dr. Higo Antonio", 8);
-        InsertMedico(db, 45, "Dr. Fadul Miguel", 8);
-        InsertMedico(db, 46, "Dra. Castillo Gabriela", 8);
-        InsertMedico(db, 47, "Dr. Novoa Pablo", 9);
-        InsertMedico(db, 48, "Dr. Orias Marcelo", 9);
-        InsertMedico(db, 49, "Dra. Barron Belen", 9);
-        InsertMedico(db, 50, "Dr. Rigo Diego", 9);
-        InsertMedico(db, 51, "Dr. Palacios Claudio", 10);
-        InsertMedico(db, 52, "Dr. Itati Zenon", 10);
-        InsertMedico(db, 54, "Dra. Faustinelli Valeria", 10);
-        InsertMedico(db, 55, "Dra. Pautaso Josefina", 10);
-        InsertMedico(db, 53, "Lic. Duarte Carolina", 11);
-        InsertMedico(db, 54, "Lic. Aragon Ana", 11);
-        InsertMedico(db, 55, "Lic. Galiano Analia", 11);
-        InsertMedico(db, 56, "Lic. Gonzalez Carolina", 11);
-        InsertMedico(db, 57, "Lic. Gonzalez Maria Victoria", 11);
-        InsertMedico(db, 58, "Dr. Vilarrodona Lucas", 12);
-        InsertMedico(db, 59, "Dr. Laje Luis", 12);
-        InsertMedico(db, 60, "Dr. Tacité Domingo", 12);
-        InsertMedico(db, 61, "Dr. Laje Poviña Luis", 12);
-        InsertMedico(db, 62, "Dr. Matach Gustavo", 12);
-        InsertMedico(db, 63, "Dr. Reviglio Victor", 12);
-        InsertMedico(db, 64, "Dr. Ale Omar", 12);
-        InsertMedico(db, 65, "Dra. Castellano Ximena", 12);
-        InsertMedico(db, 66, "Dra. Cáceres Beatriz", 12);
-        InsertMedico(db, 67, "Dr. Zernotti Mario", 13);
-        InsertMedico(db, 68, "Dra. Benitez Paula", 13);
-        InsertMedico(db, 69, "Dr. Conci Rodolfo", 13);
-        InsertMedico(db, 70, "Dr. Filiberti Gabriel", 13);
-        InsertMedico(db, 71, "Dra. Mirilo Gabriela", 13);
-        InsertMedico(db, 72, "Dr. Olmos Manuel", 13);
-        InsertMedico(db, 73, "Dra. Castro Fernanda", 13);
-        InsertMedico(db, 74, "Dr. Rey Gonzalo", 13);
-        InsertMedico(db, 75, "Dr. Bergallo Roberto", 14);
-        InsertMedico(db, 76, "Dra. Bergallo Soledad", 14);
-        InsertMedico(db, 77, "Dra. Blanco Silvana", 14);
-        InsertMedico(db, 78, "Dra. Bruno Paula", 14);
-        InsertMedico(db, 79, "Dr. Buteler Lucas", 14);
-        InsertMedico(db, 80, "Dr. Balacco Martin", 14);
-        InsertMedico(db, 81, "Dra. Becerra Adriana", 14);
-        InsertMedico(db, 82, "Dr. Allende Bartolomé Tomas", 15);
-        InsertMedico(db, 83, "Dr. Allende Gillermo José", 15);
-        InsertMedico(db, 84, "Dr. Allende Bartolomé", 15);
-        InsertMedico(db, 85, "Dr. Allende Cristian", 15);
-        InsertMedico(db, 86, "Dra. Allende Victoria", 15);
-        InsertMedico(db, 87, "Dr. Bertoloti Roman", 15);
-        InsertMedico(db, 88, "Dr. Vitar Ivan", 15);
-        InsertMedico(db, 89, "Dr. Lopez Seoane Manuel", 16);
-        InsertMedico(db, 90, "Prof. Dr. Juaneda Ricardo", 16);
-        InsertMedico(db, 91, "Dr. Barros Nores Julio", 16);
-        InsertMedico(db, 92, "Dr. Bechis Gustavo", 16);
-        InsertMedico(db, 93, "Dr. Bertran Marcelo", 16);
-        InsertMedico(db, 94, "Dr. Epelde Marcos", 16);
-        InsertMedico(db, 95, "Dr. Gonzalez Tomás", 16);
-        InsertMedico(db, 96, "Dr. Minusi Gustavo", 16);
-
+        InsertSucursal(db, 2, "CERRO");
+        InsertSucursal(db,1, "NUEVA CÓRDOBA");
 
         InsertEspecialidades(db, 1, "Alergia e inmunología");
         InsertEspecialidades(db, 2, "Anestesiología");
@@ -218,43 +139,140 @@ public class SQLiteDB extends SQLiteOpenHelper {
         InsertCobertura(db,32,"UNIMED");
         InsertCobertura(db,33,"UPCN");
 
+        InsertMedico(db, 1, "Dr. Copioli Carlos", 1, 1);
+        InsertMedico(db, 2, "Dra. Barrera Roxana", 1,1);
+        InsertMedico(db, 3, "Dra. Cavallo Marta", 1,1);
+        InsertMedico(db, 4, "Dra.Puentes Laura", 1,2);
+        InsertMedico(db, 5, "Dr. Astore Fabian",2,1);
+        InsertMedico(db, 6, "Dr. Demaria Javier", 2,1);
+        InsertMedico(db, 7, "Dr. Escudero Juan", 2,1);
+        InsertMedico(db, 8, "Dr. Gross Carlos", 2,2);
+        InsertMedico(db, 9, "Dr. Pacheco Eduardo", 2,2);
+        InsertMedico(db, 10, "Dr. Riveri Fancisco", 2,2);
+        InsertMedico(db, 11, "Dr. Salas Marcos", 2,2);
+        InsertMedico(db, 12, "Dra. Castro Maria Belen", 2,2);
+        InsertMedico(db, 13, "Dr. Moreira Eduardo", 3,1);
+        InsertMedico(db, 14, "Dr. Alday Luis", 3,1);
+        InsertMedico(db, 15, "Dr. Allall Oscar", 3,1);
+        InsertMedico(db, 16, "Dr. Allub Alejandro", 3,1);
+        InsertMedico(db, 17, "Dra. Auad Luciana", 3,2);
+        InsertMedico(db, 18, "Dr. Bass Gabriel", 3,2);
+        InsertMedico(db, 19, "Dr. Bono Julio", 3,2);
+        InsertMedico(db, 20, "Dr. Cordoba Roque", 4,1);
+        InsertMedico(db, 21, "Dr. Figueroa Marcelo", 4,1);
+        InsertMedico(db, 22, "Dr. Bronzi Jorge", 4,1);
+        InsertMedico(db, 23, "Dr. Cordoba Ignacio", 4,2);
+        InsertMedico(db, 24, "Dr. Liendo Rocardo", 4,2);
+        InsertMedico(db, 25, "Dr. Bono Julio", 5,1);
+        InsertMedico(db, 26, "Dra. Gonzales Eleonora", 5,1);
+        InsertMedico(db, 27, "Dra. Baldrini Maria Pia", 5,1);
+        InsertMedico(db, 28, "Dr. Centeno Alejandro", 5,1);
+        InsertMedico(db, 29, "Dr. Perrotat Pedro", 5,2);
+        InsertMedico(db, 30, "Dra. Ramona Griselda", 5,2);
+        InsertMedico(db, 31, "Dra. Vidal Graciaela", 5,2);
+        InsertMedico(db, 32, "Lic. Riera Monica", 6,1);
+        InsertMedico(db, 33, "Lic. Fuentes Anabella", 6,1);
+        InsertMedico(db, 34, "Lic. Leon Noelia", 6,2);
+        InsertMedico(db, 35, "Lic. Maranzana Melisa", 6,2);
+        InsertMedico(db, 36, "Dr. Ortiz Gustavo", 7,1);
+        InsertMedico(db, 37, "Dra. Arja Ana", 7,1);
+        InsertMedico(db, 38, "Dra. Bergoglio Marina", 7,1);
+        InsertMedico(db, 39, "Dra. Griguol Sandra", 7,1);
+        InsertMedico(db, 40, "Dra. Acuña Valeria", 7,2);
+        InsertMedico(db, 41, "Dr. Carri Julio", 8,1);
+        InsertMedico(db, 42, "Dra. Cordoba Claudia", 8,1);
+        InsertMedico(db, 43, "Dr. Faule Sebastian", 8,1);
+        InsertMedico(db, 44, "Dr. Higo Antonio", 8,2);
+        InsertMedico(db, 45, "Dr. Fadul Miguel", 8,2);
+        InsertMedico(db, 46, "Dra. Castillo Gabriela", 8,2);
+        InsertMedico(db, 47, "Dr. Novoa Pablo", 9,1);
+        InsertMedico(db, 48, "Dr. Orias Marcelo", 9,1);
+        InsertMedico(db, 49, "Dra. Barron Belen", 9,1);
+        InsertMedico(db, 50, "Dr. Rigo Diego", 9,1);
+        InsertMedico(db, 51, "Dr. Palacios Claudio", 10,1);
+        InsertMedico(db, 52, "Dr. Itati Zenon", 10,1);
+        InsertMedico(db, 54, "Dra. Faustinelli Valeria", 10,2);
+        InsertMedico(db, 55, "Dra. Pautaso Josefina", 10,2);
+        InsertMedico(db, 53, "Lic. Duarte Carolina", 11,1);
+        InsertMedico(db, 54, "Lic. Aragon Ana", 11,1);
+        InsertMedico(db, 55, "Lic. Galiano Analia", 11,2);
+        InsertMedico(db, 56, "Lic. Gonzalez Carolina", 11,2);
+        InsertMedico(db, 57, "Lic. Gonzalez Maria Victoria", 11,2);
+        InsertMedico(db, 58, "Dr. Vilarrodona Lucas", 12,1);
+        InsertMedico(db, 59, "Dr. Laje Luis", 12,1);
+        InsertMedico(db, 60, "Dr. Tacité Domingo", 12,2);
+        InsertMedico(db, 61, "Dr. Laje Poviña Luis", 12,2);
+        InsertMedico(db, 62, "Dr. Matach Gustavo", 12,2);
+        InsertMedico(db, 63, "Dr. Reviglio Victor", 12,2);
+        InsertMedico(db, 64, "Dr. Ale Omar", 12,2);
+        InsertMedico(db, 65, "Dra. Castellano Ximena", 12,2);
+        InsertMedico(db, 66, "Dra. Cáceres Beatriz", 12,2);
+        InsertMedico(db, 67, "Dr. Zernotti Mario", 13,1);
+        InsertMedico(db, 68, "Dra. Benitez Paula", 13,1);
+        InsertMedico(db, 69, "Dr. Conci Rodolfo", 13,1);
+        InsertMedico(db, 70, "Dr. Filiberti Gabriel", 13,1);
+        InsertMedico(db, 71, "Dra. Mirilo Gabriela", 13,1);
+        InsertMedico(db, 72, "Dr. Olmos Manuel", 13,1);
+        InsertMedico(db, 73, "Dra. Castro Fernanda", 13,2);
+        InsertMedico(db, 74, "Dr. Rey Gonzalo", 13,2);
+        InsertMedico(db, 75, "Dr. Bergallo Roberto", 14,1);
+        InsertMedico(db, 76, "Dra. Bergallo Soledad", 14,1);
+        InsertMedico(db, 77, "Dra. Blanco Silvana", 14,1);
+        InsertMedico(db, 78, "Dra. Bruno Paula", 14,2);
+        InsertMedico(db, 79, "Dr. Buteler Lucas", 14,2);
+        InsertMedico(db, 80, "Dr. Balacco Martin", 14,2);
+        InsertMedico(db, 81, "Dra. Becerra Adriana", 14,2);
+        InsertMedico(db, 82, "Dr. Allende Bartolomé Tomas", 15,1);
+        InsertMedico(db, 83, "Dr. Allende Gillermo José", 15,1);
+        InsertMedico(db, 84, "Dr. Allende Bartolomé", 15,1);
+        InsertMedico(db, 85, "Dr. Allende Cristian", 15,1);
+        InsertMedico(db, 86, "Dra. Allende Victoria", 15,2);
+        InsertMedico(db, 87, "Dr. Bertoloti Roman", 15,2);
+        InsertMedico(db, 88, "Dr. Vitar Ivan", 15,2);
+        InsertMedico(db, 89, "Dr. Lopez Seoane Manuel", 16,1);
+        InsertMedico(db, 90, "Prof. Dr. Juaneda Ricardo", 16,1);
+        InsertMedico(db, 91, "Dr. Barros Nores Julio", 16,1);
+        InsertMedico(db, 92, "Dr. Bechis Gustavo", 16,2);
+        InsertMedico(db, 93, "Dr. Bertran Marcelo", 16,2);
+        InsertMedico(db, 94, "Dr. Epelde Marcos", 16,2);
+        InsertMedico(db, 95, "Dr. Gonzalez Tomás", 16,2);
+        InsertMedico(db, 96, "Dr. Minusi Gustavo", 16,2);
 
-//HACER EL INSERT
     }
 
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
-    if(oldVersion==1 && newVersion==2) {
-        db.execSQL("DROP TABLE IF EXISTS " + MedTable);
-        db.execSQL("DROP TABLE IF EXISTS " + EspeTable);
-        db.execSQL("DROP TABLE IF EXISTS " + CobTable);
+        if(oldVersion==2 && newVersion==3) {
+            db.execSQL("DROP TABLE IF EXISTS " + MedTable);
+            db.execSQL("DROP TABLE IF EXISTS " + EspeTable);
+            db.execSQL("DROP TABLE IF EXISTS " + CobTable);
+            db.execSQL("DROP TABLE IF EXISTS "+ SucTable);
 
-        db.execSQL("DROP VIEW IF EXISTS " + viewMed);
-        db.execSQL("DROP VIEW IF EXISTS " + viewEspe);
-        db.execSQL("DROP VIEW IF EXISTS " + viewCobertura);
-        db.execSQL("DROP TRIGGER IF EXISTS  fk_medEspe_espeid");
+            db.execSQL("DROP VIEW IF EXISTS " + viewMed);
+            db.execSQL("DROP VIEW IF EXISTS " + viewEspe);
+            db.execSQL("DROP VIEW IF EXISTS " + viewCobertura);
+            db.execSQL("DROP TRIGGER IF EXISTS  fk_medEspe_espeid");
+            db.execSQL("DROP TRIGGER IF EXISTS  fk_medSuc_Sucid");
 
-        onCreate(db);
+            onCreate(db);
+        }
     }
-    }
 
-    void InsertMedico(SQLiteDatabase db, int id, String medName, int espe)
+    void InsertMedico(SQLiteDatabase db, int id, String medName, int espe, int suc)
     {
-
         ContentValues cv = new ContentValues();
         cv.put(colMedID, id);
         cv.put(colMedName, medName);
         cv.put(colMedEspe, espe);
+        cv.put(colMedSuc, suc);
         db.insert(MedTable, colMedID, cv);
-
-
     }
+
     void InsertEspecialidades(SQLiteDatabase db, int id, String descrip)
     {
         ContentValues cv = new ContentValues();
         cv.put(colEspeID, id);
         cv.put(colEspeDescrip, descrip);
         db.insert(EspeTable, colEspeID, cv);
-
     }
     void InsertCobertura(SQLiteDatabase db, int id, String cobName)
     {
@@ -262,13 +280,29 @@ public class SQLiteDB extends SQLiteOpenHelper {
         cv.put(colCobID, id);
         cv.put(colCobName, cobName);
         db.insert(CobTable, colCobID, cv);
-
+    }
+    void InsertSucursal(SQLiteDatabase db, int id, String sucName)
+    {
+        ContentValues cv = new ContentValues();
+        cv.put(colSucID, id);
+        cv.put(colSucName, sucName);
+        db.insert(SucTable, colSucID, cv);
     }
 
-
-   public Cursor getAllMedicos() {
+    public String getMedFromEspe2(int ID) {
         SQLiteDatabase db = this.getReadableDatabase();
-        Cursor cur = db.rawQuery("SELECT * FROM " + viewMed, null);
+        String[] params = new String[] { String.valueOf(ID) };
+        Cursor c = db.rawQuery("SELECT " + colMedName + colSucName + " FROM" + MedTable + SucTable +
+                " WHERE " + colMedSuc + "=" + colSucID + " AND "+ colMedEspe + "=?" , params);
+        c.moveToFirst();
+        int index = c.getColumnIndex(colMedID);
+        return c.getString(index);
+    }
+
+    public Cursor getMedicosFromEspe(int id) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cur = db.rawQuery("SELECT " + colMedName + colSucName + " FROM" + MedTable + SucTable +
+                " WHERE " + colMedSuc + "=" + colSucID + " AND "+ colMedEspe + "="+ id , null);
         return cur;
     }
 
@@ -281,7 +315,7 @@ public class SQLiteDB extends SQLiteOpenHelper {
 
     Cursor getAllCoberturas() {
         SQLiteDatabase db = this.getReadableDatabase();
-        Cursor cur = db.rawQuery("SELECT * FROM " + CobTable, null);
+        Cursor cur = db.rawQuery("SELECT * FROM " + viewCobertura, null);
 
         return cur;
     }
